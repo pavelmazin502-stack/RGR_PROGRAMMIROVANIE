43fb901b931696ec9f59505052a5d696823708f4
Pavel <pavelmazin502@gmail.com>
2026-06-11 02:11:05 +0700

    Добавлен заголовочный файл для реализации шифра Скитала

---
+++ Skitala.h
@@ -0,0 +1,8 @@
+#ifndef SKITALA_H_INCLUDED
+#define SKITALA_H_INCLUDED
+#include <iostream>
+
+std:: string ScytaleCipher(std::string key, std::string message);
+std:: string deScytaleCipher(const std::string& keyStr, const std::string& message);
+
+#endif
