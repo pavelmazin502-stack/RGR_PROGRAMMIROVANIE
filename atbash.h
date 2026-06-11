9ea19517b50de6c5ffebafdedf4b4905eb122f0a
snowball321 <ivan240507@inbox.ru>
2026-06-12 06:45:50 +0700

    добавлен заголовочный файл атбаш

---
+++ atbash.h
@@ -0,0 +1,9 @@
+#ifndef ATBASH_H
+#define ATBASH_H
+
+#include <string>
+
+// Функция для шифрования и расшифровки текста
+std::wstring apply_atbash(const std::wstring& input);
+
+#endif // ATBASH_H
