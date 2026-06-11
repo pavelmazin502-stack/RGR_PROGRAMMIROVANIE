d115cbaf2c78d4ef2ebf4d5b06f7d1df6b588021
snowball321 <ivan240507@inbox.ru>
2026-06-12 05:56:49 +0700

    лишнее

--- Skitala.h
+++
@@ -1,8 +0,0 @@
-#ifndef SKITALA_H_INCLUDED
-#define SKITALA_H_INCLUDED
-#include <iostream>
-
-std:: string ScytaleCipher(std::string key, std::string message);
-std:: string deScytaleCipher(const std::string& keyStr, const std::string& message);
-
-#endif
